b014a7d961f82ab34f3d4a986f833116fcecae81
Evgenij <kudrinevgenij@gmail.com>
2024-07-17 13:52:46 +0700

    5. Утверждения с коллекциями [#504885]

---
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.assertj;
+
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleConvertTest {
+    @Test
+    void checkArray() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        String[] array = simpleConvert.toArray("first", "second", "three", "four", "five");
+        assertThat(array).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test
+    void checkList() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        List<String> list = simpleConvert.toList("first", "second", "three", "four", "five");
+        assertThat(list).hasSize(5)
+                .contains("first","second")
+                .contains("three", Index.atIndex(2))
+                .allSatisfy(e -> assertThat(e.length()).isGreaterThan(3));
+    }
+
+    @Test
+    void checkSet() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Set<String> set = simpleConvert.toSet("first", "second", "three");
+        assertThat(set).isNotEmpty()
+                .hasSize(3)
+                .contains("first","second")
+                .allMatch(e -> e.length() < 10);
+
+    }
+
+    @Test
+    void checkMap() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Map<String, Integer> map = simpleConvert.toMap("first", "second", "three");
+        assertThat(map).isNotEmpty()
+                .hasSize(3)
+                .containsEntry("second", 1)
+                .containsKeys("first", "second");
+    }
+}
