89afb8ebdb8271199d497068ec77c2ef0dd94547
Evgenij <kudrinevgenij@gmail.com>
2024-10-25 11:27:21 +0700

    5.2. BufferedOutputStream [#252490]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.collection;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +13,14 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>(16);
+        Calendar calendar = Calendar.getInstance();
+        User user1 = new User("Alex", 5, calendar);
+        User user2 = new User("Alex", 5, calendar);
+        map.put(user1, new Object());
+        map.put(user2, new Object());
     }
 }
